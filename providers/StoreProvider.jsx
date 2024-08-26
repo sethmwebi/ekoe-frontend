df2596101d58ee6b450e10bbfdd5796f4698b5db
Seth Mwebi <sethmwebi27@gmail.com>
2024-08-26 13:59:28 +0300

    sets up redux config

--- providers/StoreProvider.jsx
+++ providers/StoreProvider.jsx
@@ -1,7 +1,24 @@
 "use client";
 import { Provider } from "react-redux";
-import { store } from "@/lib/store";
+import { makeStore } from "@/lib/store";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import { useRef } from "react";
 
 export default function StoreProvider({ children }) {
-  return <Provider store={store}>{children}</Provider>;
+  const storeRef = useRef();
+  if (!storeRef.current) {
+    storeRef.current = makeStore();
+    setupListeners(storeRef.current.dispatch);
+  }
+
+  const persistor = persistStore(storeRef.current);
+  return (
+    <Provider store={storeRef.current}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 }
